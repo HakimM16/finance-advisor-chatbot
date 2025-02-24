59868d563f4d53b262a31ad22c5810256ea5f04d
Hakim Mabike <hakimmabike2@gmail.com>
2025-02-24 16:27:18 +0000

    Added a slogan and made both images opposite sides of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,8 @@ export default function Home() {
   return (
     <main className='App'>
         <div className="header">
-          <Image src='/images/finance_advisor_logo.png' alt='logo' width={120} height={120} className={styles.logo_image}/>
+          <Image src='/images/finance_advisor_logo.png' alt='logo' width={200} height={200} className={styles.logo_image}/>
+          <Image src='/images/slogan.png' alt='slogan' width={200} height={200} className={styles.logo_image}/>
         </div>
       <div className="container">
         <Chat />
